085717a59459438a011f28b3c823fcc5263233bb
SteTisci <ste.tisci@gmail.com>
2025-10-30 04:22:14 +0100

    feat: bitMask to speed up component operations

--- src/EntityManager.ts
+++ src/EntityManager.ts
@@ -1,29 +1,26 @@
 import { ComponentName } from './types/index.js';
+import { ComponentMap } from './utils/ComponentMap.js';
 
 // The EntityManager is responsible for creating and managing entities.
 //
 // Each entity is represented by a numeric ID (eid) that is incrementally assigned.
 // Removed entities are recycled to avoid ID exhaustion.
 //
-// The manager also tracks which components each entity currently has using a Map structure.
+// It also tracks which components each entity currently has using a bitMask.
 
 export function EntityManager() {
-  const entities = new Map<number, Set<ComponentName>>();
+  const bitMasks: number[] = [];
   const recycledEids: number[] = [];
   let nextEid = 0;
 
   function exists(eid: number): boolean {
-    return entities.has(eid);
-  }
-
-  function hasComponent<T extends ComponentName>(eid: number, name: T): boolean {
-    return entities.get(eid)?.has(name) ?? false;
+    return eid >= 0 && eid < nextEid && bitMasks[eid] !== undefined;
   }
 
   function create(): number {
     const eid = recycledEids.length > 0 ? recycledEids.pop()! : nextEid++;
 
-    entities.set(eid, new Set());
+    bitMasks[eid] = 0;
     return eid;
   }
 
@@ -32,8 +29,8 @@ export function EntityManager() {
       throw new Error(`Entity ${eid} does not exist`);
     }
 
-    entities.delete(eid);
     recycledEids.push(eid);
+    delete bitMasks[eid];
   }
 
   function addComponent<T extends ComponentName>(eid: number, name: T) {
@@ -45,7 +42,8 @@ export function EntityManager() {
       throw new Error(`Entity ${eid} already has component ${name}`);
     }
 
-    entities.get(eid)!.add(name);
+    const cid = ComponentMap.getId(name);
+    bitMasks[eid] |= 1 << cid;
   }
 
   function removeComponent<T extends ComponentName>(eid: number, name: T) {
@@ -53,22 +51,15 @@ export function EntityManager() {
       throw new Error(`Entity ${eid} does not exist`);
     }
 
-    const components = entities.get(eid);
-
-    if (!components?.has(name)) {
-      throw new Error(`Entity ${eid} does not have component ${name}`);
-    }
-
-    components.delete(name);
+    const cid = ComponentMap.getId(name);
+    bitMasks[eid] &= ~(1 << cid);
   }
 
-  function getComponents(eid: number): Set<ComponentName> | undefined {
-    return entities.get(eid);
+  function hasComponent<T extends ComponentName>(eid: number, name: T) {
+    const cid = ComponentMap.getId(name);
+
+    return (bitMasks[eid] & (1 << cid)) !== 0;
   }
 
-  function getAll(): MapIterator<[number, Set<ComponentName>]> {
-    return entities.entries();
-  }
-
-  return { create, remove, addComponent, removeComponent, getComponents, getAll, exists, hasComponent };
+  return { create, remove, addComponent, removeComponent, exists, hasComponent, bitMasks };
 }
---
+++ src/utils/ComponentMap.ts
@@ -0,0 +1,35 @@
+import { ComponentName } from '../types/index.js';
+
+function ComponentRegistry() {
+  const nameToId = new Map();
+  const idToName = new Map();
+  let nextId = 0;
+
+  function register(name: ComponentName) {
+    if (nameToId.has(name)) return;
+
+    const id = nextId++;
+
+    nameToId.set(name, id);
+    idToName.set(id, name);
+  }
+
+  function getId(name: ComponentName): number {
+    return nameToId.get(name);
+  }
+
+  function getName(id: number): ComponentName {
+    return idToName.get(id);
+  }
+
+  return { register, getId, getName };
+}
+
+// Map every component to a number (max components = 32)
+
+export const ComponentMap = ComponentRegistry();
+
+ComponentMap.register('position');
+ComponentMap.register('size');
+ComponentMap.register('velocity');
+ComponentMap.register('sprite');
